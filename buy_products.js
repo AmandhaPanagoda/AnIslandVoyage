5c7ed7fff3d34885bd4f51fadf509b8d7bbd05cc
Amandha Panagoda <amandhaisiri@gmail.com>
2023-03-04 00:44:32 +0530

    corrected the add to cart the functionality

--- buy_products.js
+++ buy_products.js
@@ -1,65 +1,107 @@
+// Get the necessary elements from the HTML
 const addToCartButtons = document.querySelectorAll(".add-to-cart");
 const totalElem = document.querySelector(".total");
 const cartItemsElem = document.querySelector(".cart-items");
 const checkoutButton = document.querySelector(".checkout");
 const clearCartButton = document.querySelector(".clear-cart");
 
+// Define an array to store the items in the cart
 let cartItems = [];
 
-// add to cart button functionality
-for (let i = 0; i < addToCartButtons.length; i++) {
-  addToCartButtons[i].addEventListener("click", function () {
-    const product = this.parentElement;
-    const name = product.querySelector("h3").innerText;
-    const price = product.querySelector(".price").innerText;
-    const quantity = parseInt(product.querySelector("input").value);
+// Disable the checkout button initially
+checkoutButton.classList.add("disabled");
+checkoutButton.disabled = true;
 
-    let existingItemIndex = -1;
-    for (let j = 0; j < cartItems.length; j++) {
-      if (cartItems[j].name === name) {
-        existingItemIndex = j;
-        break;
-      }
+// Function to update the cart
+function updateCart() {
+  // Initialize variables to store the total price and cart items HTML
+  let totalPrice = 0;
+  let cartItemsHTML = "";
+  
+  // Loop through the items in the cart
+  for (let i = 0; i < cartItems.length; i++) {
+    // Get the price and quantity of the item
+    const itemPrice = parseFloat(cartItems[i].price.slice(1));
+    const itemQuantity = cartItems[i].quantity;
+    
+    // Calculate the total price of the item
+    const itemTotalPrice = itemPrice * itemQuantity;
+    
+    // Add the item's HTML to the cart items HTML
+    cartItemsHTML += `<div>${cartItems[i].name} - ${itemPrice.toFixed(2)} x ${itemQuantity} = ${itemTotalPrice.toFixed(2)}</div>`;
+    
+    // Add the item's total price to the total price
+    totalPrice += itemTotalPrice;
+  }
+  
+  // Set the total and cart items HTML in the HTML
+  totalElem.innerText = `Total: $${totalPrice.toFixed(2)}`;
+  cartItemsElem.innerHTML = cartItemsHTML;
+  
+  // Disable the checkout button if the total is 0
+  if (totalPrice === 0) {
+    checkoutButton.classList.add("disabled");
+    checkoutButton.disabled = true;
+  } else {
+    checkoutButton.classList.remove("disabled");
+    checkoutButton.disabled = false;
+  }
+}
+
+// Function to handle adding an item to the cart
+function addToCart(event) {
+  // Get the product details from the HTML
+  const product = event.target.parentElement;
+  const name = product.querySelector("h3").innerText;
+  const price = product.querySelector(".price").innerText;
+  const quantity = parseInt(product.querySelector("input").value);
+
+  // Check if the item already exists in the cart
+  let existingItemIndex = -1;
+  for (let i = 0; i < cartItems.length; i++) {
+    if (cartItems[i].name === name) {
+      existingItemIndex = i;
+      break;
     }
+  }
 
+  // Remove the item from the cart if the quantity is 0
+  if (quantity === 0 && existingItemIndex !== -1) {
+    cartItems.splice(existingItemIndex, 1);
+  }
+
+  // Add or update the item in the cart if the quantity is greater than 0
+  else if (quantity > 0) {
     if (existingItemIndex !== -1) {
       cartItems[existingItemIndex].quantity = quantity;
     } else {
       cartItems.push({ name, price, quantity });
     }
+  }
 
-    let totalPrice = 0;
-    let cartItemsHTML = "";
-    for (let j = 0; j < cartItems.length; j++) {
-      const itemPrice = parseFloat(cartItems[j].price.slice(1));
-      const itemTotalPrice = itemPrice * cartItems[j].quantity;
-      totalPrice += itemTotalPrice;
-
-      if (cartItems[j].quantity != 0) {
-        cartItemsHTML += `<div>${cartItems[j].name} - ${itemPrice.toFixed(2)} x ${cartItems[j].quantity} = ${itemTotalPrice.toFixed(2)}</div>`;
-      }   
-    }
-    totalElem.innerText = `Total: $${totalPrice.toFixed(2)}`;
-    cartItemsElem.innerHTML = cartItemsHTML;
-
-    //check out button 
-    if (totalPrice === 0) {
-      document.querySelector(".checkout").setAttribute("disabled", true);
-    } else {
-      document.querySelector(".checkout").removeAttribute("disabled");
-    }
-  });
+  // Update the cart
+  updateCart();
 }
 
 
-//clear button functionality
-clearCartButton.addEventListener("click", function () {
+
+// Add event listeners to the add to cart buttons
+for (let i = 0; i < addToCartButtons.length; i++) {
+  addToCartButtons[i].addEventListener("click", addToCart);
+}
+
+// Clear the cart including the quantity
+function clearCart() {
+  // Set the quantity of all input fields to 0
   const quantityInputs = document.querySelectorAll(".quantity");
   for (let i = 0; i < quantityInputs.length; i++) {
     quantityInputs[i].value = 0;
   }
-  cartItems = []; //clear cart
-  totalElem.innerText = "Total: $0.00";
-  cartItemsElem.innerHTML = "";
-  checkoutButton.disabled = true;
-});
+  
+  // Clear the cart array and update the cart
+  cartItems = [];
+  updateCart();
+}
+
+// event listener for clear cart button
+clearCartButton.addEventListener("click", clearCart);
