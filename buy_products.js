8a97c987ef0781c9eb827136b3e1dd03789e1818
Amandha Panagoda <amandhaisiri@gmail.com>
2023-03-18 11:08:09 +0530

    adding product contributions

--- buy_products.js
+++ buy_products.js
@@ -7,8 +7,11 @@ const clearCartButton = document.querySelector(".clear-cart");
 // array to store the items in the cart
 let cartItems = [];
 
+//checkout button disabled when loaded
 checkoutButton.classList.add("disabled");
 checkoutButton.disabled = true;
+
+clearCartButton.addEventListener("click", clearCart); //clear cart event listener
 
 for (let i = 0; i < addToCartButtons.length; i++) {
   addToCartButtons[i].addEventListener("click", addToCart);
@@ -51,13 +54,14 @@ function updateCart() {
     const itemPrice = parseFloat(cartItems[i].price.slice(1));
     const itemQuantity = cartItems[i].quantity;
     const itemTotalPrice = itemPrice * itemQuantity;
-  
+
+    //creates a new div and display cart items in the side panel
     cartItemsString += `<div>${cartItems[i].name} x ${itemQuantity} = $${itemTotalPrice.toFixed(2)}</div>`;
     
     totalPrice += itemTotalPrice;
   }
   
-  totalElem.innerText = `Total: $${totalPrice.toFixed(2)}`;
+  totalElem.innerText = `Total: $${totalPrice.toFixed(2)}`; //updates the total
   cartItemsElem.innerHTML = cartItemsString;
   
   if (totalPrice === 0) {
@@ -78,16 +82,69 @@ function updateCart() {
   }
 }
 
-clearCartButton.addEventListener("click", clearCart);
-
 function clearCart() {
+  cartItems = [];
+  updateCart();
   const quantityInputs = document.querySelectorAll(".quantity");
   for (let i = 0; i < quantityInputs.length; i++) {
     quantityInputs[i].value = 0;
   }
   document.getElementById("checkout-form").reset();
-  cartItems = [];
-  updateCart();
+}
+
+// form elements
+const userNameInput = document.getElementById("user-name");
+const contactInput = document.getElementById("contact");
+const emailInput = document.getElementById("email");
+const errorSpans = document.querySelectorAll(".error");
+
+//event listeners for blur event
+userNameInput.addEventListener("blur", validateUserName);
+contactInput.addEventListener("blur", validateContact);
+emailInput.addEventListener("blur", validateEmail);
+
+//validate the user name input
+function validateUserName() {
+  const userName = userNameInput.value.trim();
+  if (userName === "") {
+    setError(errorSpans[0], "Please enter your name");
+  } else {
+    clearError(errorSpans[0]);
+  }
+}
+
+//validate the contact input
+function validateContact() {
+  const contact = contactInput.value.trim();
+  const contactPattern = /^07[1,2,5,6,7,8][0-9]{7}$/;
+  if (!contactPattern.test(contact)) {
+    setError(errorSpans[1], "Please enter a valid contact number");
+  } else {
+    clearError(errorSpans[1]);
+  }
+}
+
+//validate the email input
+function validateEmail() {
+  const email = emailInput.value.trim();
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(email)) {
+    setError(errorSpans[2], "Please enter a valid email address");
+  } else {
+    clearError(errorSpans[2]);
+  }
+}
+
+//set an error message in the error span
+function setError(errorSpan, errorMessage) {
+  errorSpan.textContent = errorMessage;
+  errorSpan.style.display = "block";
+}
+
+//clear the error message in the error span
+function clearError(errorSpan) {
+  errorSpan.textContent = "";
+  errorSpan.style.display = "none";
 }
 
 //slides control
@@ -99,7 +156,7 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
+// image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
