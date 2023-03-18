1daf9417c5e99f0fa34eb42aab0229020d0065c5
Amandha Panagoda <amandhaisiri@gmail.com>
2023-03-18 21:54:59 +0530

    minor changes on buttons

--- content.js
+++ content.js
@@ -1,4 +1,17 @@
-//back to top button
+//back to top button functionality
+let backToTopButton = document.getElementById("back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+}
+
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
