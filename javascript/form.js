40ea4cb9d581c906314a12a89ad4a08d012946f8
Amandha Panagoda <amandhaisiri@gmail.com>
2023-04-05 20:22:29 +0530

    changed paths

---
+++ javascript/form.js
@@ -0,0 +1,52 @@
+function validate() {
+  var firstname = document.getElementById("fname").value;
+  var lastname = document.getElementById("lname").value;
+  var phone = document.getElementById("phone").value;
+  var email = document.getElementById("email").value;
+  var error_message = document.getElementById("error_message");
+  
+  error_message.style.padding = "10px";
+  
+  var text;
+  if(firstname.length < 3) {
+    text = "Please enter your first name";
+    error_message.innerHTML = text;
+    window.scrollTo(0, 0);
+    return false;
+  }
+  if(lastname.length < 3) {
+    text = "Please enter your last name";
+    error_message.innerHTML = text;
+    window.scrollTo(0, 0);
+    return false;
+  }
+  if(isNaN(phone) || phone.length != 10) {
+    text = "Please enter a valid phone number";
+    error_message.innerHTML = text;
+    window.scrollTo(0, 0);
+    return false;
+  }
+  if(email.indexOf("@") == -1 || email.length < 6) {
+    text = "Please enter a valid email";
+    error_message.innerHTML = text;
+    window.scrollTo(0, 0);
+    return false;
+  }
+  
+ 
+  openPopup();
+  return true;
+}
+
+
+$(':radio').change(function() {
+  console.log('New star rating: ' + this.value);
+});
+
+function openPopup() {
+  document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
